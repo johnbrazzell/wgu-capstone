a2334302f8ab8a35ed69ec246b6ea660c3a9fea3
John Brazzell <jbrazzellaph@gmail.com>
2021-11-23 23:48:16 -0500

    Users can now be updated
    Admin now has the ability to update a users username and admin status

--- plant-locator-tool/plant-locator-tool/EditUserWindow.xaml.cs
+++ plant-locator-tool/plant-locator-tool/EditUserWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace plant_locator_tool
 {
@@ -19,10 +20,60 @@ namespace plant_locator_tool
     /// </summary>
     public partial class EditUserWindow : Window
     {
+        private User updatedUser;
+  
+        private string oldUsername;
         public EditUserWindow(User user)
         {
             InitializeComponent();
+            if(user.AdminStatus)
+            {
+                isAdminCheckBox.IsChecked = true;
+            }
             nameOfUserLabel.Content = "Editing User " + user.UserName;
+
+    
+      
+            //Store username for DB update
+            oldUsername = user.UserName;
+            
+            updatedUser = user;
         }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            updatedUser.UserName = usernameTextBox.Text;
+            updatedUser.AdminStatus = isAdminCheckBox.IsChecked.Value;
+            
+
+            MySqlCommand command = DBHelper.GetConnection().CreateCommand();
+          
+
+            command.CommandText = "UPDATE user SET username=@username, isAdmin=@isAdmin WHERE username=@oldUsername";
+            command.Parameters.AddWithValue("@username", updatedUser.UserName);
+            command.Parameters.AddWithValue("@isAdmin", updatedUser.AdminStatus);
+            command.Parameters.AddWithValue("oldUsername", oldUsername);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (MySqlException exception)
+            {
+                MessageBox.Show(exception.ToString());
+                return; // Return to give user a chance to correct errors
+            }
+
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+  
     }
 }
